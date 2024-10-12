9b00bd97d0e517e3c82b0f01df1d10ef07ed647b
Edward Gulmayo <astaltin@gmail.com>
2024-10-12 11:13:21 +0800

    style: format file according to linting rules

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,21 +1,18 @@
 const routes = [
   {
-    path: "/",
-    component: () => import("layouts/MainLayout.vue"),
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: "", component: () => import("src/pages/DashboardPage.vue") },
+      { path: '', component: () => import('src/pages/DashboardPage.vue') },
       {
-        path: "/products",
-        component: () => import("src/pages/ProductsPage.vue"),
+        path: '/products',
+        component: () => import('src/pages/ProductsPage.vue'),
       },
     ],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
-    path: "/:catchAll(.*)*",
-    component: () => import("pages/ErrorNotFound.vue"),
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
   },
 ];
 
