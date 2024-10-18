ed42e3bd44cd7a16f490b829bba90e82337440fa
Edward Gulmayo <astaltin@gmail.com>
2024-10-18 15:12:17 +0800

    chore: update CORS configuration for allowed methods and headers

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -17,13 +17,13 @@ return [
 
     'paths' => ['*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
     'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Authorization', 'Content-Type', 'X-XSRF-Token'],
 
     'exposed_headers' => [],
 
