971c6a52d585fce2837730df1c067f3bd180ea9b
Edward Gulmayo <astaltin@gmail.com>
2024-11-01 21:27:05 +0800

    fix: add error handling to `ProductsController::index()`

--- backend/app/Http/Controllers/ProductsController.php
+++ backend/app/Http/Controllers/ProductsController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Log;
 
 class ProductsController extends Controller
 {
     public function index()
     {
-        $products = Product::with(['inventory', 'category'])->get();
+        try {
+            $products = Product::with(['inventory', 'category'])->get();
 
-        return response()->json(['products' => $products]);
+            return response()->json(['products' => $products]);
+        } catch (\Throwable $th) {
+            Log::error('ProductsController::index(): ' . $th->getMessage());
+
+            return response()->json([
+                'message' => 'Failed to load products.'
+            ], 500);
+        }
     }
 }
